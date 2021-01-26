da9652d040710d965fd867e5bcac90176ed1dcc6
tarlektarnoy <niran_tarlek@hotmail.com>
2021-01-26 22:09:04 +0700

    ีupdate

--- backend/views/salereportemp/empcomlist.php
+++ backend/views/salereportemp/empcomlist.php
@@ -354,7 +354,7 @@ function findComextrarate($emp_id, $sale_total_amt, $f_date, $t_date)
             $model_cnt = \common\models\QueryCarDailyEmpCount::find()->where(['car_id' => $modelx->car_id_])->andFilterWhere(['between', 'trans_date', $f_date, $t_date])->one();
             if($model_cnt){
                 if($model_cnt->emp_qty > 1){
-                    $c = 0;
+                    $c = 10;
                     return $c;
                 }
             }
