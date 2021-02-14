2e7bc80338c8da88891671ec416ea60e8dd0d6ed
tarlektarnoy <niran_tarlek@hotmail.com>
2021-02-14 13:18:11 +0700

    'update'

--- backend/views/orders/_form.php
+++ backend/views/orders/_form.php
@@ -392,6 +392,7 @@ $js = <<<JS
               'data': {'id': ids},
               'success': function(data) {
                    e.closest('tr').find(".select-condition").html(data);
+                   alert(e.closest('tr').find('.line-customer-id').val());
               }
          });
      }
