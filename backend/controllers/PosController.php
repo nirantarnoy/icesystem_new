ac648007484cd4beab458090513a69343b251d08
tarlektarnoy <niran_tarlek@hotmail.com>
2021-03-06 10:57:27 +0700

    'update'

--- backend/controllers/PosController.php
+++ backend/controllers/PosController.php
@@ -374,10 +374,14 @@ class PosController extends Controller
     }
 
     public function actionDailysum(){
+        $t_date = date('Y-m-d');
+
         $searchModel = new \backend\models\SaleposdataSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         $dataProvider->query->select(['code','name','SUM(qty) as qty','SUM(line_total) as line_total']);
         $dataProvider->query->andFilterWhere(['>','qty',0]);
+        $dataProvider->query->andFilterWhere(['>=','date(order_date)',$t_date]);
+
         $dataProvider->query->groupBy(['code','name']);
         $dataProvider->setSort([
             'defaultOrder'=>['code'=>SORT_ASC]
@@ -387,6 +391,7 @@ class PosController extends Controller
         $dataProvider2 = $searchModel2->search(Yii::$app->request->queryParams);
         $dataProvider2->query->select(['code','name','SUM(payment_amount) as payment_amount']);
         $dataProvider2->query->andFilterWhere(['>','payment_amount',0]);
+        $dataProvider2->query->andFilterWhere(['>=','date(payment_date)',$t_date]);
         $dataProvider2->query->groupBy(['code','name']);
         $dataProvider2->setSort([
             'defaultOrder'=>['code'=>SORT_ASC]
