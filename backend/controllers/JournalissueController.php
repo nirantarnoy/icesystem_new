7fb94ea6304029825a5cfbfdb824fdb791994ecf
tarlektarnoy <niran_tarlek@hotmail.com>
2021-03-25 19:37:39 +0700

    'update'

--- backend/controllers/JournalissueController.php
+++ backend/controllers/JournalissueController.php
@@ -24,7 +24,7 @@ class JournalissueController extends Controller
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
-                    'delete' => ['POST'],
+                    'delete' => ['POST','GET'],
                 ],
             ],
         ];
