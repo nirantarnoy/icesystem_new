f0ee6f9bbba4e37a6c2c702ad006312d1e5378b7
tarlektarnoy <niran_tarlek@hotmail.com>
2021-03-25 19:38:47 +0700

    'update'

--- backend/controllers/JournalissueController.php
+++ backend/controllers/JournalissueController.php
@@ -15,9 +15,7 @@ use yii\filters\VerbFilter;
  */
 class JournalissueController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
+   public $enableCsrfValidation = false;
     public function behaviors()
     {
         return [
