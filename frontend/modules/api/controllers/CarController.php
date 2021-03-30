75e91a17e5ebb94d76306aa22225fa3e5c8c1601
tarlektarnoy <niran_tarlek@hotmail.com>
2021-03-30 13:54:25 +0700

    'update'

---
+++ frontend/modules/api/controllers/CarController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace frontend\modules\api\controllers;
+
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+
+class CarController extends Controller
+{
+    public $enableCsrfValidation = false;
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'list' => ['POST'],
+                ],
+            ],
+        ];
+    }
+    public function actionList()
+    {
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $req_data = \Yii::$app->request->getBodyParams();
+       // $route_id = $req_data['route_id'];
+
+        $data = [];
+        $status = false;
+        //if($route_id){
+            $model = \common\models\Car::find()->all();
+            //$model = \common\models\Car::find()->where(['delivery_route_id'=>$route_id])->all();
+            if ($model) {
+                $status = true;
+                foreach ($model as $value) {
+                    array_push($data, [
+                        'id' => $value->id,
+                        'code' => $value->code,
+                        'name' => $value->name,
+                       // 'route_id' => $value->delivery_route_id
+                    ]);
+                }
+            }
+       // }
+
+        return ['status' => $status, 'data' => $data];
+    }
+}
