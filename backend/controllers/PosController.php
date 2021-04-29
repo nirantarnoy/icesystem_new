2a4531995a31dd2e554f8ff9b0d7539352f9d56d
tarlektarnoy <niran_tarlek@hotmail.com>
2021-04-29 20:53:10 +0700

    'update'

--- backend/controllers/PosController.php
+++ backend/controllers/PosController.php
@@ -536,6 +536,12 @@ class PosController extends Controller
         $order_cash_amount = \common\models\QuerySalePosPayDaily::find()->where(['created_by'=>$user_id])->andFilterWhere(['between','payment_date',$user_login_datetime,$t_date])->sum('payment_amount');
         $order_credit_amount = \common\models\QuerySalePosPayDaily::find()->where(['created_by'=>$user_id])->andFilterWhere(['between','payment_date',$user_login_datetime,$t_date])->sum('payment_amount');
         $production_qty = \backend\models\Stocktrans::find()->where(['activity_type_id'=>1])->andFilterWhere(['between','trans_date',$user_login_datetime,$t_date])->sum('qty');
+
+       echo $order_cash_qty;
+       echo $order_credit_qty;
+
+       return;
+
         return $this->render('_closesale',[
             'order_qty' => $order_qty,
             'order_amount' => $order_amount,
