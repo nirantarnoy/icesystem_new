e015fd86d1ddd8350244638975fa032f8c590aeb
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-02 17:19:42 +0700

    'update'

--- backend/views/pos/_closesale.php
+++ backend/views/pos/_closesale.php
@@ -94,121 +94,128 @@ $t_date = date('Y-m-d H:i:s');
     </div>
     <br/>
     <hr/>
+
+    <!--    <input type="submit" value="ok">-->
+
+
+    <br/>
+    <div class="row">
+        <div class="col-lg-12">
+            <table class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>สินค้า</th>
+                    <th style="text-align: right">ยอดยกมา</th>
+                    <th style="text-align: right">ยอดผลิต</th>
+                    <th style="text-align: right">ขายสด(จำนวน)</th>
+                    <th style="text-align: right">ขายเชื่อ(จำนวน)</th>
+                    <th style="text-align: right">รวม</th>
+                    <th style="text-align: right">ขายสด(เงิน)</th>
+                    <th style="text-align: right">ขายเชื่อ(เงิน)</th>
+                    <th style="text-align: right">รวม</th>
+                    <th style="text-align: right">
+                        ยอดยกไป
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($order_product_item as $value): ?>
+                    <?php
+                    $production_rec_qty = getProdDaily($value->product_id, $user_login_time, $t_date);
+                    $order_cash_qty = getOrderCashQty($value->product_id, $user_id, $user_login_datetime, $t_date);
+                    $order_credit_qty = getOrderCreditQty($value->product_id, $user_id, $user_login_datetime, $t_date);
+                    $balance_in = 0;
+                    $balance_out = 0;
+                    $order_cash_amount = 0;
+                    $order_credit_amount = 0;
+                    ?>
+                    <tr>
+                        <td style="text-align: left">
+                            <input type="hidden" name="line_prod_id[]" value="<?= $value->product_id ?>">
+                            <?= \backend\models\Product::findName($value->product_id) ?>
+                        </td>
+                        <td style="text-align: right">
+                            <input type="hidden" name="line_balance_in[]" value="<?= $balance_in ?>">
+                            <?= $balance_in ?>
+                        </td>
+                        <td style="text-align: right">
+                            <input type="hidden" name="line_production_qty[]" value="<?= $production_rec_qty ?>">
+                            <?= number_format($production_rec_qty) ?>
+                        </td>
+                        <td style="text-align: right">
+                            <input type="hidden" name="line_cash_qty[]" value="<?= $order_cash_qty ?>">
+                            <?= number_format($order_cash_qty) ?>
+                        </td>
+                        <td style="text-align: right">
+                            <input type="hidden" name="line_credit_qty[]" value="<?= $order_credit_qty ?>">
+                            <?= number_format($order_credit_qty) ?>
+                        </td>
+                        <td style="text-align: right">
+                            <?= number_format($order_cash_qty + $order_credit_qty) ?>
+                        </td>
+                        <td style="text-align: right">
+                            <input type="hidden" name="line_cash_amount[]" value="<?= $order_cash_amount ?>">
+                            <?= number_format($order_cash_amount) ?>
+                        </td>
+                        <td style="text-align: right">
+                            <input type="hidden" name="line_credit_amount[]" value="<?= $order_credit_amount ?>">
+                            <?= number_format($order_credit_amount) ?>
+                        </td>
+                        <td style="text-align: right">
+                            <?= number_format($order_cash_amount + $order_credit_amount) ?>
+                        </td>
+                        <td style="text-align: right">
+                            <?= number_format($balance_out) ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <br />
     <div class="row" style="text-align: center">
         <div class="col-lg-12">
             <div class="btn btn-success btn-lg"><i class="fa fa-save"></i> บันทึกปิดการขาย</div>
         </div>
     </div>
-    <input type="submit" value="ok">
 </form>
-
-<br/>
-<div class="row">
-    <div class="col-lg-12">
-        <table class="table table-striped table-bordered">
-            <thead>
-            <tr>
-                <th>สินค้า</th>
-                <th style="text-align: right">ยอดยกมา</th>
-                <th style="text-align: right">ยอดผลิต</th>
-                <th style="text-align: right">ขายสด(จำนวน)</th>
-                <th style="text-align: right">ขายเชื่อ(จำนวน)</th>
-                <th style="text-align: right">รวม</th>
-                <th style="text-align: right">ขายสด(เงิน)</th>
-                <th style="text-align: right">ขายเชื่อ(เงิน)</th>
-                <th style="text-align: right">รวม</th>
-                <th style="text-align: right">
-                    ยอดยกไป
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($order_product_item as $value): ?>
-                <?php
-                   $production_rec_qty = getProdDaily($value->product_id,$user_login_time,$t_date);
-                $order_cash_qty = getOrderCashQty($value->product_id,$user_id,$user_login_datetime,$t_date);
-                $order_credit_qty = getOrderCreditQty($value->product_id,$user_id,$user_login_datetime,$t_date);
-                $balance_in = 0;
-                $balance_out = 0;
-                $order_cash_amount = 0;
-                $order_credit_amount = 0;
-                ?>
-                <tr>
-                    <td style="text-align: left">
-                        <input type="hidden" name="line_prod_id[]" value="<?=$value->product_id?>">
-                        <?=\backend\models\Product::findName($value->product_id)?>
-                    </td>
-                    <td style="text-align: right">
-                        <input type="hidden" name="line_balance_in[]" value="<?=$balance_in?>">
-                        <?=$balance_in?>
-                    </td>
-                    <td style="text-align: right">
-                        <input type="hidden" name="line_production_qty[]" value="<?=$production_rec_qty?>">
-                        <?=number_format($production_rec_qty)?>
-                    </td>
-                    <td style="text-align: right">
-                        <input type="hidden" name="line_cash_qty[]" value="<?=$order_cash_qty?>">
-                        <?=number_format($order_cash_qty)?>
-                    </td>
-                    <td style="text-align: right">
-                        <input type="hidden" name="line_credit_qty[]" value="<?=$order_credit_qty?>">
-                        <?=number_format($order_credit_qty)?>
-                    </td>
-                    <td style="text-align: right">
-                        <?=number_format($order_cash_qty + $order_credit_qty)?>
-                    </td>
-                    <td style="text-align: right">
-                        <input type="hidden" name="line_cash_amount[]" value="<?=$order_cash_amount?>">
-                        <?=number_format($order_cash_amount)?>
-                    </td>
-                    <td style="text-align: right">
-                        <input type="hidden" name="line_credit_amount[]" value="<?=$order_credit_amount?>">
-                        <?=number_format($order_credit_amount)?>
-                    </td>
-                    <td style="text-align: right">
-                        <?=number_format($order_cash_amount + $order_credit_amount)?>
-                    </td>
-                    <td style="text-align: right">
-                        <?=number_format($balance_out)?>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-</div>
-
 <?php
-function getProdDaily($product_id,$user_login_datetime,$t_date){
+function getProdDaily($product_id, $user_login_datetime, $t_date)
+{
     $qty = 0;
-    if($product_id!=null){
-       $qty =\backend\models\Stocktrans::find()->where(['activity_type_id' => 1,'product_id'=>$product_id])->andFilterWhere(['between', 'trans_date', $user_login_datetime, $t_date])->sum('qty');
+    if ($product_id != null) {
+        $qty = \backend\models\Stocktrans::find()->where(['activity_type_id' => 1, 'product_id' => $product_id])->andFilterWhere(['between', 'trans_date', $user_login_datetime, $t_date])->sum('qty');
     }
 
     return $qty;
 }
 
-function getOrderCashQty($product_id,$user_id,$user_login_datetime,$t_date){
+function getOrderCashQty($product_id, $user_id, $user_login_datetime, $t_date)
+{
     $qty = 0;
-    if($user_id != null){
-        $qty = \common\models\QuerySaleDataSummary::find()->where(['created_by' => $user_id,'product_id'=>$product_id])->andFilterWhere(['between', 'order_date', $user_login_datetime, $t_date])->andFilterWhere(['LIKE','name','สด'])->sum('qty');
-    }
-
-    return $qty;
-}
-function getOrderCreditQty($product_id,$user_id,$user_login_datetime,$t_date){
-    $qty = 0;
-    if($user_id != null){
-         $qty = \common\models\QuerySaleDataSummary::find()->where(['created_by' => $user_id,'product_id'=>$product_id])->andFilterWhere(['between', 'order_date', $user_login_datetime, $t_date])->andFilterWhere(['NOT LIKE','name','สด'])->sum('qty');
+    if ($user_id != null) {
+        $qty = \common\models\QuerySaleDataSummary::find()->where(['created_by' => $user_id, 'product_id' => $product_id])->andFilterWhere(['between', 'order_date', $user_login_datetime, $t_date])->andFilterWhere(['LIKE', 'name', 'สด'])->sum('qty');
     }
 
     return $qty;
 }
 
-function getOrderCashAmount($product_id,$user_id,$user_login_datetime,$t_date){
+function getOrderCreditQty($product_id, $user_id, $user_login_datetime, $t_date)
+{
     $qty = 0;
-    if($user_id != null){
-        $qty =\common\models\QuerySalePosPayDaily::find()->where(['created_by' => $user_id])->andFilterWhere(['between', 'payment_date', $user_login_datetime, $t_date])->andFilterWhere(['LIKE','name','สด'])->sum('payment_amount');
+    if ($user_id != null) {
+        $qty = \common\models\QuerySaleDataSummary::find()->where(['created_by' => $user_id, 'product_id' => $product_id])->andFilterWhere(['between', 'order_date', $user_login_datetime, $t_date])->andFilterWhere(['NOT LIKE', 'name', 'สด'])->sum('qty');
+    }
+
+    return $qty;
+}
+
+function getOrderCashAmount($product_id, $user_id, $user_login_datetime, $t_date)
+{
+    $qty = 0;
+    if ($user_id != null) {
+        $qty = \common\models\QuerySalePosPayDaily::find()->where(['created_by' => $user_id])->andFilterWhere(['between', 'payment_date', $user_login_datetime, $t_date])->andFilterWhere(['LIKE', 'name', 'สด'])->sum('payment_amount');
     }
 
     return $qty;
