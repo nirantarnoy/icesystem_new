200c61aa2e8120ace9cf1bf92041362b8c710cef
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-09 16:55:12 +0700

    'update'

--- backend/controllers/DeliveryrouteController.php
+++ backend/controllers/DeliveryrouteController.php
@@ -15,9 +15,7 @@ use yii\filters\VerbFilter;
  */
 class DeliveryrouteController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
+   public $enableCsrfValidation = false;
     public function behaviors()
     {
         return [
