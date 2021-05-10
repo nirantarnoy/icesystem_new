5027272a435302eb523b3156d2acb0e54fd10cac
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-10 19:41:00 +0700

    'update'

--- backend/models/CardailySearch.php
+++ backend/models/CardailySearch.php
@@ -11,15 +11,16 @@ use backend\models\Cardaily;
  */
 class CardailySearch extends Cardaily
 {
-   public $route_id;
-   public $car_name;
+    public $route_id;
+    public $car_name;
+
     public function rules()
     {
         return [
             [['id', 'car_id', 'employee_id', 'is_driver', 'status', 'created_at', 'updated_at', 'created_by', 'updated_by'], 'integer'],
             [['trans_date'], 'safe'],
-            [['route_id'],'integer'],
-            [['car_name'],'string'],
+            [['route_id'], 'integer'],
+            [['car_name'], 'string'],
         ];
     }
 
@@ -41,8 +42,8 @@ class CardailySearch extends Cardaily
      */
     public function search($params)
     {
-        $query = Cardaily::find()->innerJoin('car','car.id=car_daily.car_id')
-                                 ->innerJoin('sale_group','car.sale_group_id = sale_group.id');
+        $query = Cardaily::find()->select(['car_daily.id', 'car_daily.car_id', 'car_daily.employee_id', 'sale_group.delivery_route_id', 'car.plate_number', 'car_daily.trans_date'])->innerJoin('car', 'car.id=car_daily.car_id')
+            ->innerJoin('sale_group', 'car.sale_group_id = sale_group.id');
 
         // add conditions that should always apply here
 
@@ -59,7 +60,7 @@ class CardailySearch extends Cardaily
         }
 
         // grid filtering conditions
-    //    $query->andFilterWhere([
+        //    $query->andFilterWhere([
 //            'id' => $this->id,
 //            'car_id' => $this->car_id,
 //            'employee_id' => $this->employee_id,
@@ -70,7 +71,7 @@ class CardailySearch extends Cardaily
 //            'updated_at' => $this->updated_at,
 //            'created_by' => $this->created_by,
 //            'updated_by' => $this->updated_by,
-   //     ]);
+        //     ]);
 
 //        if(isset($_SESSION['user_company_id'])){
 //            $query->andFilterWhere(['car_daily.company_id'=>$_SESSION['user_company_id']]);
@@ -79,14 +80,14 @@ class CardailySearch extends Cardaily
 //            $query->andFilterWhere(['car_daily.branch_id'=>$_SESSION['user_branch_id']]);
 //        }
 
-        if($this->route_id != null){
-            $query->andFilterWhere(['sale_group.delivery_route_id'=>$this->route_id]);
+        if ($this->route_id != null) {
+            $query->andFilterWhere(['sale_group.delivery_route_id' => $this->route_id]);
         }
-        if($this->car_name !=''){
-           // $query->andFilterWhere(['OR',['LIKE','car.name', $this->car_name],['LIKE','car.description', $this->car_name]]);
-            $query->andFilterWhere(['LIKE','car.plate_number', $this->car_name]);
+        if ($this->car_name != '') {
+            // $query->andFilterWhere(['OR',['LIKE','car.name', $this->car_name],['LIKE','car.description', $this->car_name]]);
+            $query->andFilterWhere(['LIKE', 'car.plate_number', $this->car_name]);
         }
-        if($this->trans_date != null){
+        if ($this->trans_date != null) {
             $x_date = explode('/', $this->trans_date);
             $f_date = date('Y-m-d');
             if (count($x_date) > 1) {
@@ -94,10 +95,10 @@ class CardailySearch extends Cardaily
             }
 
             $this->trans_date = date('Y-m-d', strtotime($f_date));
-            $query->andFilterWhere(['date(car_daily.trans_date)'=>$this->trans_date]);
-        }else{
+            $query->andFilterWhere(['date(car_daily.trans_date)' => $this->trans_date]);
+        } else {
             $this->trans_date = date('Y-m-d');
-            $query->andFilterWhere(['date(car_daily.trans_date)'=>$this->trans_date]);
+            $query->andFilterWhere(['date(car_daily.trans_date)' => $this->trans_date]);
         }
 
         return $dataProvider;
