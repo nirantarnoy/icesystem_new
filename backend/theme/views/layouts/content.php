f3a1b4a7d395b830bf412360589c8cfdae42daa9
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-12 21:25:46 +0700

    'update'

--- backend/theme/views/layouts/content.php
+++ backend/theme/views/layouts/content.php
@@ -4,14 +4,13 @@ $branch_id = 1;
 
 if (empty(\Yii::$app->user->identity->company_id)) {
     //return $this->redirect(['site/logout']);
-
-
     return \Yii::$app->runAction('site/logout');
 }
 if (\Yii::$app->user->identity->company_id != null) {
     $company_id = \Yii::$app->user->identity->company_id;
 } else {
     //  return $this->redirect(['site/logout']);
+    return \Yii::$app->runAction('site/logout');
 }
 if (\Yii::$app->user->identity->branch_id != null) {
     $branch_id = \Yii::$app->user->identity->branch_id;
