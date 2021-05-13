359c43f916cc50b7303a57360627647f3d075b38
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-13 09:31:52 +0700

    'update'

--- backend/models/BranchSearch.php
+++ backend/models/BranchSearch.php
@@ -70,9 +70,9 @@ class BranchSearch extends Branch
         if (!empty(\Yii::$app->user->identity->company_id)) {
             $query->andFilterWhere(['company_id' => \Yii::$app->user->identity->company_id]);
         }
-        if (!empty(\Yii::$app->user->identity->branch_id)) {
-            $query->andFilterWhere(['branch_id' => \Yii::$app->user->identity->branch_id]);
-        }
+//        if (!empty(\Yii::$app->user->identity->branch_id)) {
+//            $query->andFilterWhere(['branch_id' => \Yii::$app->user->identity->branch_id]);
+//        }
 
        if($this->globalSearch != ''){
            $query->orFilterWhere(['like', 'code', $this->globalSearch])
