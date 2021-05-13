c397005a3dbaa02b27ed5afb61f2ab3f01d8874b
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-13 18:12:40 +0700

    'update'

--- backend/views/pos/index.php
+++ backend/views/pos/index.php
@@ -11,11 +11,12 @@ $filename_do = "empty";
 
 $company_id = 1;
 $branch_id = 1;
-if (isset($_SESSION['user_company_id'])) {
-    $company_id = $_SESSION['user_company_id'];
+
+if (!empty(\Yii::$app->user->identity->company_id)) {
+    $company_id = \Yii::$app->user->identity->company_id;
 }
-if (isset($_SESSION['user_branch_id'])) {
-    $branch_id = $_SESSION['user_branch_id'];
+if (!empty(\Yii::$app->user->identity->branch_id)) {
+    $branch_id = \Yii::$app->user->identity->branch_id;
 }
 
 //echo $company_id.'<br />';
