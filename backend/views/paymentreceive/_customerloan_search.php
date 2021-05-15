94166a1a9e65cec4daf4cfd45437fdb8d906cc5c
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-15 11:20:52 +0700

    'update'

--- backend/views/paymentreceive/_customerloan_search.php
+++ backend/views/paymentreceive/_customerloan_search.php
@@ -17,15 +17,27 @@ use kartik\select2\Select2;
         ],
     ]); ?>
 
-
+    <div class="row">
+        <div class="col-lg-3">
+            <?= $form->field($model, 'car_ref_id')->widget(Select2::className(), [
+                'data' => ArrayHelper::map(\backend\models\Car::find()->where(['company_id' => $company_id, 'branch_id' => $branch_id])->all(), 'id', 'code'),
+                'options' => [
+                    'placeholder' => 'เลือกรถ'
+                ],
+                'pluginOptions' => [
+                        'multiple'=> true,
+                ]
+            ])->label() ?>
+        </div>
+        <div class="col-lg-3"></div>
+        <div class="col-lg-3"></div>
+        <div class="col-lg-3">
+            <input type="submit" class="btn btn-primary" value="ค้นหา">
+        </div>
+    </div>
     <div class="input-group">
         <!--         <span class="input-group-addon" id="basic-addon1"><i class="fa fa-search"></i></span>-->
-        <?= $form->field($model, 'car_ref_id')->widget(Select2::className(), [
-            'data' => ArrayHelper::map(\backend\models\Car::find()->where(['company_id' => $company_id, 'branch_id' => $branch_id])->all(), 'id', 'code'),
-            'options' => [
-                'placeholder' => 'เลือกรถ'
-            ]
-        ])->label(false) ?>
+
     </div>
     <?php ActiveForm::end(); ?>
 
