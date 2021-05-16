ce4e34ed5d2b77ffec6d8e6b96ba66461f02646f
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-16 17:33:44 +0700

    'update'

--- frontend/modules/api/controllers/OrderController.php
+++ frontend/modules/api/controllers/OrderController.php
@@ -737,8 +737,9 @@ class OrderController extends Controller
         if ($route_id != null && $order_date != null) {
          //   $model = \backend\models\Orders::find()->where(['order_channel_id' => $route_id, 'date(order_date)' => $f_date])->andFilterWhere(['<', 'status', 100])->one();
             $model = \backend\models\Orders::find()->where(['order_channel_id' => $route_id, 'date(order_date)' => $trans_date])->one();
-            $data = ['route_id'=>$route_id,'data'=>$trans_date];
+
             if ($model) {
+                $data = ['route_id'=>$route_id,'data'=>$trans_date];
 //                $data = ['route_id'=>$route_id,'data'=>$order_date];
 //                $model_close = \common\models\QuerySaleFinished::find()->where()->all();
 //                if ($model_close) {
