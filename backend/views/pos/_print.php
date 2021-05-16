acb571eb6f4875c97fc5c205a307395484a8931e
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-16 16:20:02 +0700

    'update'

--- backend/views/pos/_print.php
+++ backend/views/pos/_print.php
@@ -9,7 +9,7 @@ $fontData = $defaultFontConfig['fontdata'];
 $mpdf = new \Mpdf\Mpdf(['tempDir' => __DIR__ . '/tmp',
 //$mpdf = new \Mpdf\Mpdf([
     //'tempDir' => '/tmp',
-    'mode' => 'utf-8', 'format' => [76, 120],
+    'mode' => 'utf-8', 'format' => [70, 120],
     'fontdata' => $fontData + [
             'sarabun' => [ // ส่วนที่ต้องเป็น lower case ครับ
                 'R' => 'THSarabunNew.ttf',
@@ -23,7 +23,7 @@ $mpdf = new \Mpdf\Mpdf(['tempDir' => __DIR__ . '/tmp',
 //$mpdf->SetMargins(-10, 1, 1);
 //$mpdf->SetDisplayMode('fullpage');
 $mpdf->AddPageByArray([
-    'margin-left' => 3,
+    'margin-left' => 5,
     'margin-right' => 2,
     'margin-top' => 0,
     'margin-bottom' => 1,
