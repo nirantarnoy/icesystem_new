504097c0e1af32fe224486be7652a7559819df75
tarlektarnoy <niran_tarlek@hotmail.com>
2021-05-17 22:01:22 +0700

    'update'

--- backend/models/Orders.php
+++ backend/models/Orders.php
@@ -224,27 +224,29 @@ class Orders extends \common\models\Orders
         }
         return $total;
     }
+
 //
-    public static function findordercredit($order_id)
+    public static function findordercredit($order_id, $sale_type)
     {
         $total = 0;
-        $model = \backend\models\Orderline::find()->where(['order_id' => $order_id])->all();
-        if ($model) {
-            foreach ($model as $value) {
-                $cus_pay_method = \backend\models\Customer::findPayMethod($value->customer_id);
-//                $paymethod_name = \backend\models\Paymentmethod::findName($cus_pay_method);
-//                if($paymethod_name == 'เงินเชื่อ' || $paymethod_name == 'เครดิต'){
-//                    $total = $total + ($value->qty * $value->price);
-//                }
-                $paymethod_id = \backend\models\Paymentmethod::find()->where(['id' => $cus_pay_method])->one();
-                if ($paymethod_id) {
-                    if ($paymethod_id->pay_type == 2) {
-                        $total = $total + ($value->qty * $value->price);
+        if ($sale_type == 1) {
+            $total = \common\models\QueryApiOrderDailySummary::find()->where(['id' => $order_id, 'sale_payment_method_id' => 2])->sum('line_total');
+        } else {
+            $model = \backend\models\Orderline::find()->where(['order_id' => $order_id])->all();
+            if ($model) {
+                foreach ($model as $value) {
+                    $cus_pay_method = \backend\models\Customer::findPayMethod($value->customer_id);
+                    $paymethod_id = \backend\models\Paymentmethod::find()->where(['id' => $cus_pay_method])->one();
+                    if ($paymethod_id) {
+                        if ($paymethod_id->pay_type == 2) {
+                            $total = $total + ($value->qty * $value->price);
+                        }
                     }
                 }
-            }
 
+            }
         }
+
 //        $model = \common\models\QuerySaleCustomerPaySummary::find()->where(['order_ref_id'=>$order_id])->sum('payment_amount');
 //        if($model){
 //            $total = $model;
