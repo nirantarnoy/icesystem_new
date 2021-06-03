a24010fbaea2c7725770e396d4eed2b558126b5a
tarlektarnoy <niran_tarlek@hotmail.com>
2021-06-03 13:36:25 +0700

    'update'

--- frontend/modules/api/controllers/CustomerController.php
+++ frontend/modules/api/controllers/CustomerController.php
@@ -17,6 +17,7 @@ class CustomerController extends Controller
                 'class' => VerbFilter::className(),
                 'actions' => [
                     'list' => ['POST'],
+                    'assetlist' => ['POST'],
                 ],
             ],
         ];
@@ -44,6 +45,37 @@ class CustomerController extends Controller
                         'code' => $value->code,
                         'name' => $value->name,
                         'route_id' => $value->delivery_route_id
+                    ]);
+                }
+            }
+        }
+
+        return ['status' => $status, 'data' => $data];
+    }
+    public function actionAssetlist()
+    {
+        $company_id = 1;
+        $branch_id = 1;
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $req_data = \Yii::$app->request->getBodyParams();
+        $customer_id = $req_data['customer_id'];
+        $company_id = $req_data['company_id'];
+        $branch_id = $req_data['branch_id'];
+
+        $data = [];
+        $status = false;
+        if ($customer_id) {
+            $model = \common\models\CustomerAsset::find()->where(['customer_id' => $customer_id, 'company_id' => $company_id, 'branch_id' => $branch_id])->all();
+            if ($model) {
+                $status = true;
+                foreach ($model as $value) {
+                    array_push($data, [
+                        'id' => $value->id,
+                        'code' => \backend\models\Product::findCode($value->product_id),
+                        'name' => \backend\models\Product::findName($value->product_id),
+                        'qty' => $value->qty,
+                        'status' => $value->status,
+                        'photo' => '',
                     ]);
                 }
             }
