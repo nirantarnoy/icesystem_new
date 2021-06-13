dcbb011223a952b41cb1bc0d9c0f914105a147cf
tarlektarnoy <niran_tarlek@hotmail.com>
2021-06-13 13:16:56 +0700

    'update'

--- frontend/modules/api/controllers/ProductionController.php
+++ frontend/modules/api/controllers/ProductionController.php
@@ -17,7 +17,8 @@ class ProductionController extends Controller
                 'class' => VerbFilter::className(),
                 'actions' => [
                     'addprodrec' => ['POST'],
-                    'warehouselist' =>['POST'],
+                    'warehouselist' => ['POST'],
+                    'addreprocesswip' => ['POST'],
                 ],
             ],
         ];
@@ -45,26 +46,26 @@ class ProductionController extends Controller
 
         if ($product_id && $warehouse_id && $qty) {
             $model_journal = new \backend\models\Stockjournal();
-            $model_journal->journal_no = $model_journal->getLastNo($company_id,$branch_id);
+            $model_journal->journal_no = $model_journal->getLastNo($company_id, $branch_id);
             $model_journal->trans_date = date('Y-m-d');
             $model_journal->company_id = $company_id;
             $model_journal->branch_id = $branch_id;
-            if($model_journal->save(false)){
-                    $model = new \backend\models\Stocktrans();
-                    $model->journal_no = $model_journal->journal_no;
-                    $model->journal_id = $model_journal->id;
-                    $model->trans_date = date('Y-m-d H:i:s');
-                    $model->product_id = $product_id;
-                    $model->qty = $qty;
-                    $model->warehouse_id = $warehouse_id;
-                    $model->stock_type = 1;
-                    $model->activity_type_id = 15; // 15 prod rec
-                    $model->company_id = $company_id;
-                    $model->branch_id = $branch_id;
-                    if($model->save()){
-                        $status = 1;
-                        $this->updateSummary($product_id,$warehouse_id,$qty);
-                    }
+            if ($model_journal->save(false)) {
+                $model = new \backend\models\Stocktrans();
+                $model->journal_no = $model_journal->journal_no;
+                $model->journal_id = $model_journal->id;
+                $model->trans_date = date('Y-m-d H:i:s');
+                $model->product_id = $product_id;
+                $model->qty = $qty;
+                $model->warehouse_id = $warehouse_id;
+                $model->stock_type = 1;
+                $model->activity_type_id = 15; // 15 prod rec
+                $model->company_id = $company_id;
+                $model->branch_id = $branch_id;
+                if ($model->save()) {
+                    $status = 1;
+                    $this->updateSummary($product_id, $warehouse_id, $qty);
+                }
             }
             $model = \backend\models\Stockjournal::find()->where(['id' => $model_journal->id])->one();
             $model_line = \backend\models\Stocktrans::find()->where(['journal_id' => $model_journal->id])->all();
@@ -73,18 +74,20 @@ class ProductionController extends Controller
             $session->setFlash('msg-index', 'slip_prodrec_index.pdf');
             $session->setFlash('after-save', true);
 
-          //  $this->renderPartial('_printtoindex', ['model' => $model, 'model_line' => $model_line, 'change_amount' => 0]);
+            //  $this->renderPartial('_printtoindex', ['model' => $model, 'model_line' => $model_line, 'change_amount' => 0]);
         }
 
         return ['status' => $status, 'data' => $data];
     }
-    public function updateSummary($product_id, $wh_id, $qty){
-        if($wh_id != null && $product_id != null && $qty > 0){
-            $model = \backend\models\Stocksum::find()->where(['warehouse_id'=>$wh_id,'product_id'=>$product_id])->one();
-            if($model){
+
+    public function updateSummary($product_id, $wh_id, $qty)
+    {
+        if ($wh_id != null && $product_id != null && $qty > 0) {
+            $model = \backend\models\Stocksum::find()->where(['warehouse_id' => $wh_id, 'product_id' => $product_id])->one();
+            if ($model) {
                 $model->qty = ($model->qty + (int)$qty);
                 $model->save(false);
-            }else{
+            } else {
                 $model_new = new \backend\models\Stocksum();
                 $model_new->warehouse_id = $wh_id;
                 $model_new->product_id = $product_id;
@@ -93,6 +96,7 @@ class ProductionController extends Controller
             }
         }
     }
+
     public function actionWarehouselist()
     {
         $company_id = 0;
@@ -106,7 +110,7 @@ class ProductionController extends Controller
         $status = false;
 
         if ($company_id) {
-            $model = \common\models\Warehouse::find()->where(['company_id'=>$company_id,'branch_id'=>$branch_id])->all();
+            $model = \common\models\Warehouse::find()->where(['company_id' => $company_id, 'branch_id' => $branch_id])->all();
             // $model = \common\models\QueryCustomerPrice::find()->all();
             if ($model) {
                 $status = true;
@@ -125,5 +129,48 @@ class ProductionController extends Controller
 
         return ['status' => $status, 'data' => $data];
     }
+
+    public function actionAddreprocesswip()
+    {
+        $company_id = 0;
+        $branch_id = 0;
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $req_data = \Yii::$app->request->getBodyParams();
+        $product_id = $req_data['product_id'];
+        $warehouse_id = $req_data['warehouse_id'];
+        $qty = $req_data['qty'];
+        $reason = $req_data['reason'];
+        $company_id = $req_data['company_id'];
+        $branch_id = $req_data['branch_id'];
+
+        $data = [];
+        $status = false;
+
+        if ($company_id != null && $product_id != null && $warehouse_id != null && $branch_id != null) {
+            $model = new \common\models\IssueReprocess();
+            $model->journal_no = 'IW-';
+            $model->trans_date = date('Y-m-d H:i:s');
+            $model->status = 1;
+            $model->created_at = time();
+            $model->created_by = 1;
+            $model->reason = $reason;
+            $model->company_id = $company_id;
+            $model->branch_id = $branch_id;
+            if($model->save(false)){
+                $model_line = new \common\models\IssueReprocessLine();
+                $model_line->issue_id = $model->id;
+                $model_line->product_id = $product_id;
+                $model_line->to_warehouse_id = 0;
+                $model_line->qty = $qty;
+                $model_line->status = 1;
+                if($model_line->save(false)){
+                    $status = 1;
+                }
+            }
+        }
+
+        return ['status' => $status, 'data' => $data];
+    }
+
 
 }
