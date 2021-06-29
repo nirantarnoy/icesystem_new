ba1d2220d1da50c569f21559cae2f3b2c3a0c30a
tarlektarnoy <niran_tarlek@hotmail.com>
2021-06-29 09:23:50 +0700

    'upda'te

--- backend/views/plan/update.php
+++ backend/views/plan/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Plan */
 
-$this->title = 'แก้ไขแผนผลิต: ' . $model->id;
+$this->title = 'แก้ไขแผนผลิต: ' . $model->journal_no;
 $this->params['breadcrumbs'][] = ['label' => 'แผนผลิต', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->journal_no, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'แก้ไข';
 ?>
 <div class="plan-update">
