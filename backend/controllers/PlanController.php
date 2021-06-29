c64b23be3dc328f525e0b83c818eb00742a3558f
tarlektarnoy <niran_tarlek@hotmail.com>
2021-06-29 09:36:38 +0700

    'upda'te

--- backend/controllers/PlanController.php
+++ backend/controllers/PlanController.php
@@ -15,9 +15,7 @@ use yii\filters\VerbFilter;
  */
 class PlanController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
+   public $enableCsrfValidation = false;
     public function behaviors()
     {
         return [
