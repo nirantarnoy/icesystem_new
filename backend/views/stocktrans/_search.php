2f4312dab40f04bca3c87dd490a910f670f23ea2
tarlektarnoy <niran_tarlek@hotmail.com>
2021-08-03 09:25:04 +0700

    'update'

--- backend/views/stocktrans/_search.php
+++ backend/views/stocktrans/_search.php
@@ -7,10 +7,10 @@ $company_id = 0;
 $branch_id = 0;
 
 if (!empty(\Yii::$app->user->identity->company_id)) {
-    $company_id = \Yii::$app->user->identity->company_id];
+    $company_id = \Yii::$app->user->identity->company_id;
 }
 if (!empty(\Yii::$app->user->identity->branch_id)) {
-    $branch_id= \Yii::$app->user->identity->branch_id];
+    $branch_id= \Yii::$app->user->identity->branch_id;
 }
 ?>
 
